5e226c133d30a8c1e8073b235880c2186c92f989
Umair-progs <umair.pandit60@gmail.com>
2024-09-07 13:07:13 +0530

    C++ template

---
+++ contest.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+//LCM FUNCTION
+long long gcd(long long int a, long long int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+
+// Function to return LCM of two numbers 
+long long lcm(long long int a,long long int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+//not understood it very well though
+void solve() {
+	int n;
+	cin>>n;
+	vector<int>flowers(n);
+
+	for(int i=0; i<n; i++)cin>>flowers[i];
+	
+	int ans=flowers[n-1];
+
+	for(int i=n-2; i>=0; i--)
+	{
+		ans=max(ans+1,flowers[i]);
+	}
+
+	cout<<ans<<endl;
+
+}
+
+int main() {
+  // optimizing
+  // ios::sync_with_stdio(false);
+  // cin.tie(nullptr);
+// #ifndef ONLINE_JUDGE
+//   freopen_s("input.txt", "r", stdin);
+//   freopen_s("output.txt", "w", stdout);
+// #endif
+  int t = 1;
+  cin>>t;
+  while (t--) {
+    solve();
+    // break;
+  }
+
+  return 0;
+}
