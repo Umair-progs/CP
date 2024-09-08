c0e492265ef3e275e402dde82bd8767e9495b813
Umair-progs <umair.pandit60@gmail.com>
2024-09-08 21:09:05 +0530

    Park Lighting

---
+++ ParkLighting.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+//LCM FUNCTION
+long long gcd(long long int a, long long int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+
+// Function to return LCM of two numbers 
+long long lcm(long long int a,long long int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+//not understood it very well though
+void solve() {
+    int n,m;
+    cin>>n>>m;
+
+    cout<<(int)floor(((float)n*(float)m+1)/(float)2)<<endl;
+}
+
+int main() {
+  // optimizing
+  // ios::sync_with_stdio(false);
+  // cin.tie(nullptr);
+// #ifndef ONLINE_JUDGE
+//   freopen_s("input.txt", "r", stdin);
+//   freopen_s("output.txt", "w", stdout);
+// #endif
+  int t = 1;
+  cin>>t;
+  while (t--) {
+    solve();
+    // break;
+  }
+
+  return 0;
+}
